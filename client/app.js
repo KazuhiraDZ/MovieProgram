bd360c0b63e67761d70e080474294484537eedaf
KazuhiraDZ <547880914@qq.com>
2018-10-09 15:13:59 +0800

    1.0.0

---
+++ client/app.js
@@ -0,0 +1,88 @@
+//app.js
+var qcloud = require('./vendor/wafer2-client-sdk/index')
+var config = require('./config')
+
+let userInfo
+
+App({
+    onLaunch: function () {
+        qcloud.setLoginUrl(config.service.loginUrl)
+    },
+    login({ success, error }) {
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo'] === false) {
+            // 已拒绝授权
+            wx.showModal({
+              title: '提示',
+              content: '请授权我们获取您的用户信息',
+              showCancel: false,
+            })
+          } else {
+            this.doQcloudLogin({ success, error })
+          }
+        }
+      })
+    },
+    doQcloudLogin({ success, error }) {
+      // 调用 qcloud 登陆接口
+      qcloud.login({
+        success: result => {
+          if (result) {
+            let userInfo = result
+            success && success({
+              userInfo
+            })
+          console.log('success!')
+          } else {
+            // 如果不是首次登录，不会返回用户信息，请求用户信息接口获取
+            this.getUserInfo({ success, error })
+            console.log('reading!')
+          }
+        },
+        fail: res => {
+          console.log(res)
+          console.log('login error!')
+          error && error()
+        }
+      })
+    },
+
+    getUserInfo({ success, error }) {
+      qcloud.request({
+        url: config.service.requestUrl,
+        login: true,
+        success: result => {
+          let data = result.data
+          if (!data.code) {
+            let userInfo = data.data
+
+            success && success({
+              userInfo
+            })
+          } else {
+            error && error()
+          }
+        },
+        fail: () => {
+          error && error()
+        }
+      })
+    },
+
+
+    checkSession({ success, error }) {
+      wx.checkSession({
+        success: () => {
+          this.getUserInfo({ success, error })
+        },
+        fail: () => {
+          error && error()
+        }
+      })
+    },
+
+
+
+
+})
